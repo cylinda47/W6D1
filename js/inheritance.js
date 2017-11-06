f7f5cfa6c43ca631fd949e6fded778f65fe44c16
Linda Chan <cylinda0407@gmail.com>
2017-11-06 15:16:42 -0800

    Complete inheritance exercise

---
+++ js/inheritance.js
@@ -0,0 +1,19 @@
+// Function.prototype.inherits = function (parent) {
+//   const Surrogate = function () {};
+//   Surrogate.prototype = parent.prototype;
+//   this.prototype = new Surrogate();
+//   this.prototype.constructor = this;
+// };
+
+Function.prototype.inherits = function (parent) {
+  this.prototype = Object.create(parent.prototype);
+  this.prototype.constructor = this;
+};
+
+function MovingObject () {}
+
+function Ship () {}
+Ship.inherits(MovingObject);
+
+function Asteroid () {}
+Asteroid.inherits(MovingObject);
